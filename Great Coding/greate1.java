2a55a51bb3b452f20482298b96b71a3d098cc154
LongBro <1459892910@qq.com>
2019-03-19 11:02:39 +0800

    为great1.java类添加注释

--- Great Coding/greate1.java
+++ Great Coding/greate1.java
@@ -8,13 +8,16 @@ import java.util.Map;
  *
  */
 public class Test {
+	//定义map的key
 	static String s[]={"name","sex","birth","age"};
 
 	public static void main(String[] args){
 		dataExport();
 	}
 	public static List dataExport(){
+		//定义map的List
 		List<Map<String,String>> list=new ArrayList<Map<String,String>>();
+		//定义二维数组的Map的Value
 		String person[][]={
 				{"ZhaoYongXuan","male","1973-10-16"},
 				{"LianXiangping","female","1972-10-16"},
@@ -25,15 +28,15 @@ public class Test {
 		for(int i=0;i<person.length;i++){
 			Map<String,String> map=new HashMap<String, String>();
 			for(int j=0;j<s.length;j++){
-				if(j!=3){
+				if(j!=3){//前三个value从二维数组中取出
 					map.put(s[j], person[i][j]);			
-				}else{//年龄是计算来的，而非在数组中
+				}else{//第四个value年龄是计算来的，而非在数组中
 					map.put(s[j], (2019-Integer.parseInt(person[i][j-1].substring(0, 4)))+"");
 				}
 			}
-			list.add(map);
+			list.add(map);//将map添加进list
 		}		
-		return list;
+		return list;//返回list
 	}
 
 }
